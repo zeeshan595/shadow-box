c54ae72094f6537d9f05984e2037bc75c0330286
Zeeshan Abid <zeeshan.abid.com@gmail.com>
2024-06-12 22:16:32 +0100

    feat: magic items now support new lines

--- src/components/item.vue
+++ src/components/item.vue
@@ -11,18 +11,30 @@ const props = defineProps<{
   <span class="italic font-small">{{ props.value.flavourText }}</span>
   <span v-if="props.value.bonus">
     <span class="font-small bold uppercase">bonus: </span>
-    {{ props.value.bonus }}
+    <template v-for="line in props.value.bonus.split('\n')">
+      <span>{{ line }}</span>
+      <br />
+    </template>
   </span>
   <span v-if="props.value.benefit">
     <span class="font-small bold uppercase">benefit: </span>
-    {{ props.value.benefit }}
+    <template v-for="line in props.value.benefit.split('\n')">
+      <span>{{ line }}</span>
+      <br />
+    </template>
   </span>
   <span v-if="props.value.curse">
     <span class="font-small bold uppercase">curse: </span>
-    {{ props.value.curse }}
+    <template v-for="line in props.value.curse.split('\n')">
+      <span>{{ line }}</span>
+      <br />
+    </template>
   </span>
   <span v-if="props.value.personality">
     <span class="font-small bold uppercase">personality: </span>
-    {{ props.value.personality }}
+    <template v-for="line in props.value.personality.split('\n')">
+      <span>{{ line }}</span>
+      <br />
+    </template>
   </span>
 </template>
